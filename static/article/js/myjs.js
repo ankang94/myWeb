d88777fbade06cbfd7a38b5131861beac43b392c
ankang94 <1439231032@qq.com>
2018-04-03 11:33:40 +0800

    page bar

--- static/article/js/myjs.js
+++ static/article/js/myjs.js
@@ -1,25 +1,13 @@
 //初始化加载
 $(function () {
-    // $('.article_body').load('/static/my/page/article.html')
-    // $('.url_param_group').attr('href', '/?param_group=' + $('.url_param_group').text())
-
-    // $('#roottitle>li').on('click', function (e) {
-    //     if (window.location.pathname == '') {
-    //         e.preventDefault();
-    //     }
-    //     $('#roottab').text = this.text();
-    //     $(this).addClass('active').siblings().removeClass('active');
-    //     $(this).find('ul').find('li').on('click', function(e) {
-    //        $(this).parent('.dropdown-menu').prev('.dropdown-toggle').text($(this).text());
-    //     });
-    // });
+    // realizehight();
     $('#pageLimit').bootstrapPaginator({
         currentPage: 1,//当前的请求页面。
         totalPages: 20,//一共多少页。
         size: "normal",//应该是页眉的大小。
         bootstrapMajorVersion: 3,//bootstrap的版本要求。
         alignment: "right",
-        numberOfPages: 5,//一页列出多少数据。
+        numberOfPages: 10,//一页列出多少数据。
         itemTexts: function (type, page, current) {//如下的代码是将页眉显示的中文显示我们自定义的中文。
             switch (type) {
                 case "first":
@@ -35,4 +23,10 @@ $(function () {
             }
         }
     });
+    
+    // function realizehight() {
+    // 25
+    //     var totalpage = ($(document).height() - 300) / ($('.catlog-grid .list-group a:first').height() || 45);
+    //     alert(Math.ceil(totalpage))
+    // }
 });
