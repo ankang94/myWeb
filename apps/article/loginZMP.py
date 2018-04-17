790ac3d87906c1aaf1d5f37f57364ffa951157a5
ankang94 <1439231032@qq.com>
2018-04-17 17:36:31 +0800

    modify fun name

--- apps/article/loginZMP.py
+++ apps/article/loginZMP.py
@@ -21,13 +21,13 @@ class LoginZMP:
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
         try:
             # 获取任务单内容
-            self.getTaskContent(arg)
+            self.get_task_content(arg)
         except:
             pass
         finally:
             self.driver.close()
 
-    def loginCookie(self):
+    def login_cookie(self):
         username = '***'
         password = '***'
         self.driver.get('https://oa.ztesoft.com')
@@ -41,7 +41,7 @@ class LoginZMP:
         self.driver.execute_script(js)
         pickle.dump(self.driver.get_cookies(), open("cookies.pkl", "wb"), 0)
 
-    def getTaskContent(self, arg):
+    def get_task_content(self, arg):
         url = 'https://oa.ztesoft.com/queryTransDtl.action?transid=' + \
               arg + '&orgState=O&language=zh_CN'
         if os.path.exists("cookies.pkl"):
@@ -50,19 +50,19 @@ class LoginZMP:
             for cookie in cookies:
                 self.driver.add_cookie(cookie)
         else:
-            self.loginCookie()
+            self.login_cookie()
         self.driver.get(url)
         if 'Login.jsp' in self.driver.current_url:
             # cookie过期重新生成
             self.driver.delete_all_cookies()
             os.remove("cookies.pkl")
-            self.loginCookie()
+            self.login_cookie()
             self.driver.get(url)
-        taskContent = WebDriverWait(self.driver, 10).until(
+        target = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(
                 # (By.XPATH, ".//*[@class='UnderRow'][2]/td[2]/span/table/tbody/tr[12]/td[2]")))
                 (By.ID, "recordList")))
-        self.result = taskContent.get_attribute('outerHTML')
+        self.result = target.get_attribute('outerHTML')
         # output = open('提交列表.txt', 'w')
         # output.writelines([line +'\n' for line in taskContent.text.split('<br/>')])
         # output.flush()
