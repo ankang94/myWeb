6c881897bbf47d8d8388fdb2d32e6395eea1339b
ankang94 <1439231032@qq.com>
2018-05-21 20:44:12 +0800

    更新从CSDN博客拷贝文章功能

--- apps/clone/views.py
+++ apps/clone/views.py
@@ -16,66 +16,50 @@ from myWeb.adapter import Cache
 pic_tmp_path = os.path.join(settings.MEDIA_ROOT, 'tmp')
 
 
-# def test_index(request):
-#     import pickle
-#     import os
-#     from selenium.webdriver.common.by import By
-#     from selenium.webdriver.support.ui import WebDriverWait
-#     from selenium.webdriver.support import expected_conditions as EC
-#     from clone.base import COOKIE_DIR
-#     ret = {}
-#     cookie_dir = os.path.join(COOKIE_DIR, 'zmp.pkl')
-#
-#     class LoginZMP:
-#         def perform(self):
-#             url = 'https://oa.ztesoft.com/queryTransDtl.action?transid=' + \
-#                   str(self.args) + '&orgState=O&language=zh_CN'
-#             if os.path.exists(cookie_dir):
-#                 self.driver.get('https://oa.ztesoft.com/Login.jsp')
-#                 cookies = pickle.load(open(cookie_dir, "rb"))
-#                 for cookie in cookies:
-#                     self.driver.add_cookie(cookie)
-#             else:
-#                 self.login_cookie()
-#             self.driver.get(url)
-#             if 'Login.jsp' in self.driver.current_url:
-#                 # cookie过期重新生成
-#                 self.driver.delete_all_cookies()
-#                 os.remove(cookie_dir)
-#                 self.login_cookie()
-#                 self.driver.get(url)
-#
-#         def login_cookie(self):
-#             username = '***'
-#             password = '***'
-#             self.driver.get('https://oa.ztesoft.com')
-#             # 显示等待登陆加载
-#             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
-#                 (By.XPATH, ".//*[@id='fm_login']/table/tbody/tr[2]/td/table")))
-#             js = 'document.getElementById(\"edt_username\").value = ' + '\"' + username + '\"' + \
-#                  ';document.getElementById(\"edt_pwd\").value = ' + '\"' + password + \
-#                  '\"' + ';document.getElementById(\"edt_ck\").checked=\"checked\"' + \
-#                  ';document.getElementById(\"login_btn\").click()'
-#             self.driver.execute_script(js)
-#             pickle.dump(self.driver.get_cookies(), open(cookie_dir, "wb"), 0)
-#
-#     if request.POST and request.POST['taskId']:
-#         task_id = request.POST['taskId']
-#         if not Cache().get(task_id):
-#             config = Config({'args': task_id})
-#             soup = create_soup(True, config, LoginZMP)
-#
-#             def filter_tag_with_parent(tag):
-#                 return tag.name == 'td' and re.match('(U|A)\s{3}branches', tag.text)
-#
-#             uptext = soup.find(filter_tag_with_parent)
-#             task_list = [str(x) for x in uptext.get_text('\n', 'br/').split('\n')] if uptext else None
-#             if task_id and len(task_id) > 0:
-#                 Cache(task_id, task_list, 60 * 15)
-#             ret['copy_list'] = task_list
-#         else:
-#             ret['copy_list'] = Cache().get(task_id)
-#     return render(request, 'copy_article.html', ret)
+def login_csdn(param):
+    import pickle
+    import os
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from clone.base import COOKIE_DIR
+    cookie_dir = os.path.join(COOKIE_DIR, 'csdn.pkl')
+
+    class LoginCSDN:
+        def perform(self):
+            url = str(self.args)
+            if os.path.exists(cookie_dir):
+                self.driver.get('https://passport.csdn.net/account/login')
+                cookies = pickle.load(open(cookie_dir, "rb"))
+                for cookie in cookies:
+                    self.driver.add_cookie(cookie)
+            else:
+                self.login_cookie()
+            self.driver.get(url)
+            if 'passport.csdn.net' in self.driver.current_url:
+                # cookie过期重新生成
+                self.driver.delete_all_cookies()
+                os.remove(cookie_dir)
+                self.login_cookie()
+                self.driver.get(url)
+
+        def login_cookie(self):
+            self.driver.get('https://passport.csdn.net/account/login')
+            # 显示等待登陆加载
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+                (By.CLASS_NAME, "login-part")))
+            username = '***'
+            password = '***'
+            js = "$('.login-part:first > h3 > a').click();\
+                $('#username').val('" + username + "');\
+                $('#password').val('" + password + "');\
+                $('#fm1').submit();"
+            self.driver.execute_script(js)
+            pickle.dump(self.driver.get_cookies(), open(cookie_dir, "wb"), 0)
+
+    config = Config({'args': param})
+    soup = create_soup(True, config, LoginCSDN)
+    return soup
 
 
 def __init_clone_template():
@@ -140,11 +124,11 @@ def clone_web_article(request):
                 'title'), request.POST.get('shost'), request.POST.get('h2'), request.POST.get('h3'), request.POST.get(
                 'context'), request.POST.get('code')
 
-            config = Config({
-                'headers': {'Host': host},
-                'url': url,
-            })
-            soup = create_soup(config=config)
+            # config = Config({
+            #     'headers': {'Host': host},
+            #     'url': url,
+            # })
+            soup = login_csdn(url)
             temp_copy_article = {'url': url}
             # title
             article_title = soup.select(title)  # 表字段
