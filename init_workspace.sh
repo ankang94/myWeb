3354aed3894b3afe31d480750f13e08c2d1df457
ankang94 <1439231032@qq.com>
2018-07-22 14:12:26 +0800

    创建必要的目录结构

---
+++ init_workspace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+WORK_HOME=`pwd`
+# touch $WORK_HOME/myWeb/base.py
+
+mkdir -p $WORK_HOME/media/ext
+mkdir -p $WORK_HOME/media/frame
+mkdir -p $WORK_HOME/media/img
+mkdir -p $WORK_HOME/media/title
+mkdir -p $WORK_HOME/media/tmp
