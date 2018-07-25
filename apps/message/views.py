00d0127697974edf14588d9de1e3af42772e76ab
ankang94 <1439231032@qq.com>
2018-07-25 23:35:26 +0800

    留言板添加分页功能

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, HttpResponse
 from myWeb.adapter import Ajax, Method
-import json
+import json, math
 
 
 def message_list(request):
-    param = {'articleId': request.GET.get('articleId'), "pageNum": "1", "pageSize": "50"}
+    param = {'articleId': request.GET.get('articleId'), "pageNum": request.GET.get('pageNum'), "pageSize": "10"}
     result = Ajax.connect('/message/list', Method.GET, param)
     if result.get('success'):
-        return render(request, 'page/message_template.html', {'ret': result.get('data')})
+        pagination = result.get('data').get('pagination')
+        page_max = int(math.ceil(pagination.get('total') / pagination.get('size')))
+        start = int(pagination.get('current') / 5) + 1
+        offset = pagination.get('current') % 5
+        end = start + 4 if start + 4 < page_max else page_max
+        prev_status = 'disabled' if start in range(1, 6) else ''
+        next_status = 'disabled' if end in range(page_max + offset - 5, page_max + 1) else ''
+        return render(request, 'page/message_template.html', {'ret': result.get('data'),
+                                                              'pages': range(start, end + 1),
+                                                              'offset': offset,
+                                                              'ps': prev_status,
+                                                              'ns': next_status})
 
 
 def message_add(request):
